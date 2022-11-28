a507bbb33eeef044570bfd54363274bd677ccc64
Igor Bezanovic <i.bezanovic@levi9.com>
2022-11-28 17:19:54 +0100

    Fixed login logic wit removed env file

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,47 +1,51 @@
-import React, { useState } from 'react'
-import Headline from 'components/Headline';
-import Input from 'components/InputField';
+import React, { useState } from "react";
+import Headline from "components/Headline";
+import Input from "components/InputField";
 import Footer from "components/Footer";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import styles from "./styles.module.css";
-import services from "../../services/product.services";
-import TwitterIcon from '@mui/icons-material/Twitter';
+import services from "../../services/auth.services";
+import BaseHttpService from "../../services/base-http.service";
+import jwt_decode from "jwt-decode";
 
 type userDTO = {
-  username: string, 
-  password: string
-}
+  username: string;
+  password: string;
+};
+
+type decodedUser = {
+  iat: number;
+  sub: number;
+  user: string;
+};
 
 const Login = () => {
   const [isError, setIsError] = useState<boolean>(false);
   const [loginUserDTO, setLoginUserDTO] = useState<userDTO>({
-    username: '', 
-    password: '',
+    username: "johnd",
+    password: "m38rmF$",
   });
+
+  const apiClient = BaseHttpService();
 
   const handleLogin = async (event: any) => {
     event.preventDefault();
 
-    // if(!loginUserDTO.username) return [console.log('Username is empty'), setIsError(true)]
-    // if(!loginUserDTO.password) return [console.log('Password is empty'), setIsError(true)]
+    if (!loginUserDTO.username)
+      return [console.log("Username is empty"), setIsError(true)];
+    if (!loginUserDTO.password)
+      return [console.log("Password is empty"), setIsError(true)];
 
     try {
-      console.log('uslismo')
-      const response = await services.loginUser({
-        username: "mor_2314",
-        password: "83r5^_"
-      })
-      console.log(await services.loginUser({
-        username: "mor_2314",
-        password: "83r5^_"
+      const response = await services.loginUser(loginUserDTO);
+      apiClient.saveToken(response.data.token);
+      const decoded: decodedUser = jwt_decode(response.data.token);
+      decoded && localStorage.setItem('username', decoded.user);
 
-      }))
-      console.log(loginUserDTO)
-      console.log('---', response.token);
       setLoginUserDTO({
-        username: '', 
-        password: '',
-      })
+        username: "",
+        password: "",
+      });
     } catch (error) {
       if (typeof error === "string") {
         console.error(error.toUpperCase());
@@ -49,32 +53,41 @@ const Login = () => {
         console.error(error.message);
       }
     }
-  }
+  };
 
   return (
     <div className={styles.loadingWrapper}>
-      <Headline title='Login' />
+      <Headline title="Login" />
       <div className={styles.displayWrapper}>
-        <TwitterIcon color="action" />
-          <Input 
-            label="Username" 
-            error={!loginUserDTO.username && isError}
-            onChange={(e: any) => setLoginUserDTO({...loginUserDTO,
-              username: e.target.value
-            })}
-            />
-          <Input
-            label="Password" 
-            error={!loginUserDTO.password && isError}
-            onChange={(e: any) => setLoginUserDTO({...loginUserDTO,
-              password: e.target.value
-            })}
-            />
-          <Button variant="outlined" onClick={handleLogin}>Submit</Button>
+        <Input
+          label="Username"
+          error={!loginUserDTO.username && isError}
+          onChange={(e: any) =>
+            setLoginUserDTO({ ...loginUserDTO, username: e.target.value })
+          }
+        />
+        <Input
+          label="Password"
+          error={!loginUserDTO.password && isError}
+          onChange={(e: any) =>
+            setLoginUserDTO({ ...loginUserDTO, password: e.target.value })
+          }
+        />
+        <Button variant="outlined" onClick={handleLogin}>
+          Submit
+        </Button>
+        <p>
+          User for testing purposes: <br />
+          <em>
+            username:'johnd',
+            <br />
+            password:'m38rmF$',
+          </em>
+        </p>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
