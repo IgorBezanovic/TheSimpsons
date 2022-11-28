5476061494141a3ce220f09b6e2a418fdbfdc9a7
Igor Bezanovic <i.bezanovic@levi9.com>
2022-11-28 19:27:14 +0100

    Login form and logic is done, left over for this feature is toast react when used type incorrect credentials

--- src/components/InputField/Input.tsx
+++ src/components/InputField/Input.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import styles from "./styles.module.css";
 
-const Input = ({ label, error = false, onChange }: { label: string, error: boolean, onChange: (e: any) => void }) => {
+const Input = ({ label, error = false, onChange, type }: { label: string, error: boolean, onChange: (e: any) => void, type: string }) => {
   const isError = error ? error : false;
 
   return (
     <TextField
       error={isError}
       id={`outlined-error-helper-text_${label}`}
-      type={label === 'Password' ? 'password' : 'text'}
+      type={type}
       label={label}
       helperText={isError && 'Incorrect entry.'}
       className={styles.marginBottom30}
       onChange={onChange}
+      fullWidth
     />
   )
 }
